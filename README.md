b32ee122bc12257e9b495da22b20f34cdd56114a
n4t5ru <hello@nasru.me>
2022-04-25 16:02:31 +0500

    Added new features to script, updated readme

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # Linux-to-AD
 
 ## About
-Script to join Windows ActiveDirectory domain with a linux machine/system
+
+Script to join Windows ActiveDirectory domain with a linux machine/system.
+
+Some other features has also added:
+
+- Add new user as Developer with SUDO access
+- Install OhmyZSH as the default shell
+- update and upgrade the required files
+- Enable UFW and add rules to allow SSH (Default port 22)
 
 ## Before Running
+
 Give the script the required permissions.
+`sudo chmod +x linux-to-ad.sh`
+
+- Make sure to run the script with `sudo` access
+`sudo ./linux-to-ad.sh`
--- linux-to-ad.sh
+++ linux-to-ad.sh
@@ -6,31 +6,27 @@
 printf '\nWelcome to Linux-to-AD\n'
 printf 'Choose your Distro
 1. Debian
-2. RPM
-3. Arch\n
+2. RPM\n
 Enter Number: \n'
 read number
 
 case $number in
 #for debian based distros
 1) 
-echo 'Enter your domain Name:'
-read domainName
-echo 'Enter your Hostname Name:'
-read hostName
-echo 'Enter Domain Admin User'
-read domainAdmin
+read -p 'Enter your domain Name: ' domainName
+read -p 'Enter your Hostname Name: ' hostName
+read -p 'Enter Domain Admin User: ' domainAdmin
 
 echo 'You will be prompted to enter domain admin password in a bit....'
 
 #change hostname to user preference
 hostnamectl set-hostname $hostName.$domainName
 
-#install the initial required tools
-apt install -y realmd libnss-sss libpam-sss sssd sssd-tools adcli samba-common-bin oddjob oddjob-mkhomedir packagekit
+#updates and upgrades all files before installation
+apt update && apt upgrade -y
 
-#join the doiman
-realm join -v -U $domanAdmin $domainName
+#install the initial required tools
+apt install -y realmd libnss-sss libpam-sss sssd sssd-tools adcli samba-common-bin oddjob oddjob-mkhomedir packagekit zsh
 
 cat <<EOF >> /usr/share/pam-configs/mkhomedir 
 Name: activate mkhomedir
@@ -43,17 +39,46 @@ EOF
 
 pam-auth-update
 
+echo 'You will be prompted to enter domain admin password in a bit....'
+
+#join the doiman
+realm join -v -U $domanAdmin $domainName
+
 systemctl restart sssd
+
+read -p "Enter New Username: " newUser
+read -s -p "Enter Password: " newPassword
+
+#checks if the username already exists
+egrep "^$newUser" /etc/passwd >/dev/null
+
+#adds new user if the username doesnot exist the in passwd file
+if [ $? -eq 0 ]; then
+		echo "$username exists!"
+		exit 1
+else
+    pass=$(perl -e 'print crypt($ARGV[0], "password")' $newPassword)
+    useradd -m -p "$pass" "$username"
+    [ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+fi
+
+#Install OhmyZSH
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#Enable UFW and add rules
+ufw enable
+ufw allow ssh
+ufw allow 22
+
+echo 'All operations completed successfully!'
+
 ;;
 
 #for RPM based distros
 2)
-echo 'Enter your domain Name:'
-read domainName
-echo 'Enter your Hostname Name:'
-read hostName
-echo 'Enter Domain Admin User'
-read domainAdmin
+read 'Enter your domain Name: ' domainName
+read 'Enter your Hostname Name: ' hostName
+read 'Enter Domain Admin User: ' domainAdmin
 
 echo 'You will be prompted to enter domain admin password in a bit....'
 
@@ -67,11 +92,6 @@ yum install -y realmd libnss-sss libpam-sss sssd sssd-tools adcli samba-common-b
 realm join -v -U $domanAdmin $domainName
 
 systemctl restart sssd
-;;
-
-3)
-#meaning this is a work in progress.
-echo 'Why do you wanna join a domain with Arch....'
 ;;
 
 *)
